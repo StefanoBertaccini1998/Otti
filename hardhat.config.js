cd7ba5fc5ef98fbeb9cf77dc06f333798579a2dc
2_S_B <s.bertacciniDev@gmail.com>
2023-12-15 11:17:06 +0100

    Fix for config doc-gen and gar report

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,55 @@
 require("@nomicfoundation/hardhat-toolbox");
 require('@openzeppelin/hardhat-upgrades');
+require("dotenv").config();
+
+const { SEPOLIA_URL, METAMASK_PRIVATE_KEY, ETHERSCAN_API_KEY, ETH_URL } =
+  process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: ETH_URL,
+        //blockNumber: 15000000,
+      },
+    },
+    dashboard: {
+      url: "http://localhost:24012/rpc",
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+    env: "./env",
+  },
+  mocha: {
+    timeout: 40000,
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    coinmarketcap: "",
+    currency: "EUR",
+    gasPriceApi: "https://api.etherscan.io/api?module=proxy&action=gasPrice",
+    token: "ETH",
+  },
+  docgen: {
+    path: "./docs",
+    clear: true,
+    runOnCompile: true,
+  }
 };
